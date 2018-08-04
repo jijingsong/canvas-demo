d827694f9c3e65a7f48af54851ce415e36c70267
jijingsong <787827925@qq.com>
2018-08-04 23:29:57 +0800

    触屏设备兼容‘ git status git commit -m 触屏设备兼容‘

--- main.js
+++ main.js
@@ -1,49 +1,78 @@
 var canvas = document.getElementById('canvas');
 var c = canvas.getContext('2d');
 
-function fullWidth() {
-    canvas.width = document.documentElement.clientWidth
-    canvas.height = document.documentElement.clientHeight
-}
-
 fullWidth()
 window.onresize = function () {
     fullWidth()
 }
 
+listenToUser(canvas)
+function listenToUser(canvas) {
+    var using = false
+    var beforePoint = { x: undefined, y: undefined }
 
-var using = false
-var beforePoint = { x: 'undifined', y: 'undifined' }
-canvas.onmousedown = function (e) {
-    var x = e.clientX
-    var y = e.clientY
-    using = true
+    if (document.body.ontouchstart !== undefined) {
+        canvas.ontouchstart = function (e) {
+            var x = e.touches[0].clientX
+            var y = e.touches[0].clientY
+            using = true
 
-    if (eraserEnabled) {
-        c.clearRect(x, y, 10, 10)
+            if (eraserEnabled) {
+                c.clearRect(x, y, 10, 10)
+            } else {
+                beforePoint = { x: x, y: y }
+            }
+
+        }
+        canvas.ontouchmove = function (e) {
+            var x = e.touches[0].clientX
+            var y = e.touches[0].clientY
+            if (!using) { return }
+
+            if (eraserEnabled) {
+                c.clearRect(x, y, 10, 10)
+            } else {
+                var afterPoint = { x: x, y: y }
+                drawLine(beforePoint.x, beforePoint.y, afterPoint.x, afterPoint.y)
+                beforePoint = afterPoint
+            }
+
+        }
+        canvas.ontouchend = function (e) {
+            using = false
+        }
     } else {
-        beforePoint = { x: x, y: y }
+        canvas.onmousedown = function (e) {
+            var x = e.clientX
+            var y = e.clientY
+            using = true
+
+            if (eraserEnabled) {
+                c.clearRect(x, y, 10, 10)
+            } else {
+                beforePoint = { x: x, y: y }
+            }
+
+        }
+        canvas.onmousemove = function (e) {
+            var x = e.clientX
+            var y = e.clientY
+            if (!using) { return }
+
+            if (eraserEnabled) {
+                c.clearRect(x, y, 10, 10)
+            } else {
+                var afterPoint = { x: x, y: y }
+                drawLine(beforePoint.x, beforePoint.y, afterPoint.x, afterPoint.y)
+                beforePoint = afterPoint
+            }
+
+        }
+        canvas.onmouseup = function (e) {
+            using = false
+        }
     }
-
 }
-canvas.onmousemove = function (e) {
-    var x = e.clientX
-    var y = e.clientY
-    if (!using) { return }
-
-    if (eraserEnabled) {
-        c.clearRect(x, y, 10, 10)
-    } else {
-        var afterPoint = { x: x, y: y }
-        drawLine(beforePoint.x, beforePoint.y, afterPoint.x, afterPoint.y)
-        beforePoint = afterPoint
-    }
-
-}
-canvas.onmouseup = function (e) {
-    using = false
-}
-
 
 var eraserEnabled = false
 eraser.onclick = function () {
@@ -55,6 +84,11 @@ draw.onclick = function () {
     actions.className = 'actions'
 }
 
+
+function fullWidth() {
+    canvas.width = document.documentElement.clientWidth
+    canvas.height = document.documentElement.clientHeight
+}
 
 function drawLine(x1, y1, x2, y2) {
     c.beginPath()
